f07bd80271bc972a774a8f2504e5973e937057b9
Daniel Stokes <kupomail@gmail.com>
2015-07-03 21:17:16 -0700

    Add a simple floor.

--- src/main.py
+++ src/main.py
@@ -5,6 +5,9 @@ class Sigurd(ShowBase):
 	def __init__(self):
 		ShowBase.__init__(self)
 
+		level_np = base.loader.loadModel("models/level")
+		level_np.reparent_to(base.render)
+
 
 if __name__ == '__main__':
 	app = Sigurd()
