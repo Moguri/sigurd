be32c2ac18ee2110a41e82b3b70c3938be654df1
Mitchell Stokes <mogurijin@gmail.com>
2015-07-03 23:02:19 -0700

    Adding a basic ECS module.

---
+++ src/ecs.py
@@ -0,0 +1,66 @@
+import weakref
+import itertools
+
+class Component(object):
+    __slots__ = [
+        "_entity",
+        "typeid",
+    ]
+
+    @property
+    def entity(self):
+        return self._entity()
+
+class Entity(object):
+    __slots__ = [
+        "_components",
+    ]
+
+    def __init__(self):
+        self._components = {}
+
+    def add_component(self, component):
+        if component.typeid in self._components:
+            raise RuntimeError("Entity already has component of with typeid of {}".format(component.typeid))
+        component.entity = weakref.ref(self)
+        self._components[component.typeid] = component
+
+    def remove_component(self, component):
+        del self._components[component.typeid]
+
+    def get_component(self, typeid):
+        return self._components[typeid]
+
+
+class System(object):
+    __slots__ = [
+        "component_types",
+    ]
+
+    def update(dt, entities):
+        pass
+
+class ECSManager(object):
+    def __init__(self):
+        self.entities = []
+        self.systems = set()
+
+    def add_entity(self, entity):
+        self.entities.append(entity)
+
+    def remove_entity(self, entity):
+        self.entities.remove(entity)
+
+    def add_system(self, system):
+        self.systems.add(system)
+
+    def remove_system(self, system):
+        self.systems.remove(system)
+
+    def update(self, dt):
+        for system in self.systems:
+            components = [component for entity in self.entities for component in self.entities._components if component.typeid in system.component_types]
+            components = dict(itertools.groupby(entities, lambda x: x.typeid))
+
+            system.update(dt, components)
+
--- src/main.py
+++ src/main.py
@@ -1,15 +1,36 @@
 #!/usr/bin/env python2
 from direct.showbase.ShowBase import ShowBase
+import panda3d.core as p3d
 
+import ecs
+
+class NodePathComponent(ecs.Component):
+    __slots__ = [
+        "nodepath",
+    ]
+
+    def __init__(self, modelpath=None):
+        if modelpath is not None:
+            self.nodepath = base.loader.loadModel(modelpath)
+        else:
+            self.nodepath = p3d.NodePath(p3d.PandaNode('node'))
 
 class Sigurd(ShowBase):
-	def __init__(self):
-		ShowBase.__init__(self)
+    def __init__(self):
+        ShowBase.__init__(self)
+    
+        self.ecsmanager = ecs.ECSManager()
+        def run_ecs(task):
+            self.ecsmanager.update(0)
+            task.cont
+        self.taskMgr.add(run_ecs, 'ECS')
 
-		level_np = base.loader.loadModel("models/level")
-		level_np.reparent_to(base.render)
-
+        level = ecs.Entity()
+        np_component = NodePathComponent('models/level')
+        np_component.nodepath.reparent_to(base.render)
+        base.render.ls()
+        self.ecsmanager.add_entity(level)
 
 if __name__ == '__main__':
-	app = Sigurd()
-	app.run()
+    app = Sigurd()
+    app.run()
