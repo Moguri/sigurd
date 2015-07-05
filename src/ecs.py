1e588bbb7f714d58fcf7fe81678a23a909fc23e5
Mitchell Stokes <mogurijin@gmail.com>
2015-07-05 12:19:35 -0700

    ECS: Systems now have a chance to initialize components.
    System.init_components() is called with all new components before they
    are used in System.update().

--- src/ecs.py
+++ src/ecs.py
@@ -16,29 +16,44 @@ class Component(object):
 class Entity(object):
     __slots__ = [
         '_components',
+        '_new_components',
         '__weakref__',
     ]
 
     def __init__(self):
         self._components = {}
+        self._new_components = {}
 
     def add_component(self, component):
-        if component.typeid in self._components:
-            raise RuntimeError('Entity already has component of with typeid of {}'.format(component.typeid))
+        if component.typeid in self._components or component.typeid in self._new_components:
+            raise RuntimeError('Entity already has component with typeid of {}'.format(component.typeid))
         component._entity = weakref.ref(self)
-        self._components[component.typeid] = component
+        self._new_components[component.typeid] = component
 
     def remove_component(self, component):
-        del self._components[component.typeid]
+        if component.typeid in self._components:
+            del self._components[component.typeid]
+        elif component.typeid in self._new_components:
+            del self._new_components[component.typeid]
+        else:
+            raise KeyError('Enity has no component with typeid of {}'.format(component.typeid))
 
     def get_component(self, typeid):
-        return self._components[typeid]
+        if typeid in self._components:
+            return self._components[typeid]
+        elif typeid in self._new_components:
+            return self._new_components
+        else:
+            raise KeyError('Enity has no component with typeid of {}'.format(typeid))
 
 
 class System(object):
     __slots__ = [
         'component_types',
     ]
+
+    def init_components(self, dt, entities):
+        pass
 
     def update(self, dt, entities):
         pass
@@ -61,9 +76,19 @@ class ECSManager(object):
     def remove_system(self, system):
         self.systems.remove(system)
 
+    def _get_components_by_type(self, component_types):
+        components = [component for entity in self.entities for component in entity._components.values() if component.typeid in component_types]
+        components = dict(itertools.groupby(components, lambda x: x.typeid))
+
+        return components
+
     def update(self, dt):
         for system in self.systems:
-            components = [component for entity in self.entities for component in entity._components.values() if component.typeid in system.component_types]
-            components = dict(itertools.groupby(components, lambda x: x.typeid))
+            system.init_components(dt, self._get_components_by_type(system.component_types))
 
-            system.update(dt, components)
+        for entity in self.entities:
+            entity._components.update(entity._new_components)
+            entity._new_components.clear()
+
+        for system in self.systems:
+            system.update(dt, self._get_components_by_type(system.component_types))
