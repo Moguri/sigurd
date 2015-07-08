363f7f8b1756b636c60cadcdb48877c0e164b29d
Daniel Stokes <kupomail@gmail.com>
2015-07-07 21:23:22 -0700

    Physics: Add ray_cast function to system.

--- src/physics.py
+++ src/physics.py
@@ -4,6 +4,24 @@ import panda3d.bullet as bullet
 import ecs
 
 
+class HitResult(object):
+    def __init__(self, bullet_hit):
+        self.position = bullet_hit.get_hit_pos()
+        self.normal = bullet_hit.get_hit_normal()
+        self.node = bullet_hit.get_node()
+        self.t = bullet_hit.get_hit_fraction()
+        self.triangle_index = bullet_hit.get_triangle_index()
+
+    def __repr__(self):
+        return '<HitResult position:{} normal:{} node:{} t:{} triangle_index:{}'.format(
+            self.position,
+            self.normal,
+            self.node,
+            self.t,
+            self.triangle_index
+        )
+
+
 class HitBoxComponent(ecs.Component):
     __slots__ = [
         'physics_node',
@@ -44,4 +62,32 @@ class PhysicsSystem(ecs.System):
         for hit_box in components.get('PHY_HITBOX', []):
             np_component = hit_box.entity.get_component('NODEPATH')
             np_component.nodepath.attach_new_node(hit_box.physics_node)
-            self.physics_world.attach_ghost(hit_box.physics_node)
+            self.physics_world.attach(hit_box.physics_node)
+
+    def ray_cast(self, from_pos, to_pos, all_hits=False, mask=None):
+        hits = []
+
+        # Draw debug lines
+        # lineseg = p3d.LineSegs('debug ray')
+        # lineseg.reset()
+        # lineseg.move_to(from_pos)
+        # lineseg.draw_to(to_pos)
+        # debug_line = lineseg.create(False)
+        # base.render.attach_new_node(debug_line)
+
+        if all_hits:
+            if mask:
+                bhits = self.physics_world.ray_test_all(from_pos, to_pos, mask)
+            else:
+                bhits = self.physics_world.ray_test_all(from_pos, to_pos)
+            for bhit in bhits.get_hits():
+                hits.append(HitResult(bhit))
+        else:
+            if mask:
+                bhit = self.physics_world.ray_test_closest(from_pos, to_pos, mask)
+            else:
+                bhit = self.physics_world.ray_test_closest(from_pos, to_pos)
+            if bhit.has_hit():
+                hits.append(HitResult(bhit))
+
+        return hits
